fabd96db44b96efd6a195dc317816ad12a38ab45
Tej Kiran Yabaku <40194753+TejkiranYabaku@users.noreply.github.com>
2023-11-26 00:27:29 -0700

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,7 +2,7 @@ async function main() {
   const Voting = await ethers.getContractFactory("Voting");
 
   // Start deployment, returning a promise that resolves to a contract object
-  const Voting_ = await Voting.deploy(["Sarah Johnson", "Olivia Davis", "Samuel Wilson", "Christopher Kim","Maya Gupta"], 90);
+  const Voting_ = await Voting.deploy(["Sarah Johnson", "Olivia Davis", "Samuel Wilson", "Christopher Kim","Maya Gupta"], 5);
   console.log("Contract address:", Voting_.address);
 
 
